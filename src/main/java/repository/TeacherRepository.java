bbe28d122912d29ac765007a0d41c3355dc19cb3
Sofia Suarez <sofiasa@ufps.edu.co>
2025-05-09 07:10:57 -0500

    teacher repositorio completado

--- src/main/java/repository/TeacherRepository.java
+++ src/main/java/repository/TeacherRepository.java
@@ -4,7 +4,9 @@ import entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
-
+    Optional<Teacher> findByEmail(String email);
 }
